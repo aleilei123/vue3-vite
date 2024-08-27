2598998f21f916b3665987cf5d4cbf27cc1ef164
aleilei123 <1395549036@qq.com>
2024-08-27 16:50:06 +0800

    feat: 新增tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
   plugins: [
     vue(),
     vueJsx(),
